70bebaffff1207a7ff7bf1b0c6b41b0dfc776560
Jake Soenneker <jake@soenneker.com>
2025-04-07 10:41:52 -0500

    Initial commit

---
+++ src/ConfigurationLoggingExtension.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog.Events;
+using Soenneker.Extensions.String;
+
+namespace Soenneker.Extensions.Configuration.Logging;
+
+/// <summary>
+/// A collection of helpful IConfiguration logging related extension methods
+/// </summary>
+public static class ConfigurationLoggingExtension
+{
+    /// <summary>
+    /// Retrieves the default Serilog <see cref="LogEventLevel"/> from the configuration.
+    /// It first attempts to read <c>Log:Levels:Default</c>, falling back to <c>Log:DefaultLogLevel</c> if not present or invalid.
+    /// If both are missing or unparsable, it defaults to <see cref="LogEventLevel.Verbose"/>.
+    /// </summary>
+    /// <param name="config">The configuration instance to read from.</param>
+    /// <returns>The resolved <see cref="LogEventLevel"/> from the configuration, or <see cref="LogEventLevel.Verbose"/> if unavailable.</returns>
+    public static LogEventLevel GetLogEventLevel(this IConfiguration config)
+    {
+        return config["Log:Levels:Default"].TryToEnum<LogEventLevel>()
+               ?? config.GetValue<string>("Log:DefaultLogLevel").TryToEnum<LogEventLevel>()
+               ?? LogEventLevel.Verbose;
+    }
+}
---
+++ test/Soenneker.Extensions.Configuration.Logging.Tests/ConfigurationLoggingExtensionTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Extensions.Configuration.Logging.Tests;
+
+public sealed class ConfigurationLoggingExtensionTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
